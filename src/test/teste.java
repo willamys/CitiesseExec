48aa45588e4160d39cf786640c7fc1b9ea383099
Willamys Araujo <willamysaraujo7@gmail.com>
2012-04-27 15:56:05 -0300

    tentando conflito
    Signed-off-by: Willamys Araujo <willamysaraujo7@gmail.com>

--- src/test/teste.java
+++ src/test/teste.java
@@ -3,7 +3,8 @@ package test;
 public class teste {
 	public static void main (String agrs[]){
 		
-		System.out.println("Teste");
+		System.out.println("Teste33233232");
 		System.out.println("Teste100");
+		System.out.println("Teste3");
 	}
 }
